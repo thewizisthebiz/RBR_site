c3a577c5860101c88ae218f901e866f4ac0f105a
thewizisthebiz <joshua.kruszynski@gmail.com>
2018-02-17 13:21:36 -0500

    WIP

--- js/main.js
+++ js/main.js
@@ -1,30 +1,5 @@
 $(document).ready(function() {
 
-	$('.c-topNav__toggle').click(function(){
-		$('.c-topNav').toggleClass('open');
-
-		if($('c-topNav__toggle').hasClass('open') == false){
-			var scrollSpot = $(window).scrollTop();
-		}
-	});
-
-	$(window).scroll(function () {
-		var imgHeight = $('.c-homeTopImage').height();
-		var scrollTop = $(window).scrollTop();
-		var navTop = imgHeight - scrollTop;
-
-		if (scrollTop < imgHeight) {
-		  $('.c-topNav').removeClass('topLocked');
-		  $('main').removeClass('topLocked');
-		}
-		if (scrollTop > imgHeight) {
-		  $('.c-topNav').addClass('topLocked');
-		  $('main').addClass('topLocked');
-		}
-	});
-
-//	var whichNav
-
 	$('[data-nav]').on('click', (event) => {
 	  $(event.target).siblings('[data-dropdown]')
 	    .toggleClass('show');
